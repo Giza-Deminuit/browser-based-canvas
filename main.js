24e7d33419398f6e137bd544f2b172d0d017e3a9
Maggie Pettus <gizademinuit@gmail.com>
2023-04-02 21:42:19 -0500

    Dynamic board size and input validation

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
+// *************************************************
+//         Populates the sketch board
+// *************************************************
 function populateBoard(size)
 {
     let board = document.querySelector('.board');
+    let squares = board.querySelectorAll('div');
+
+    squares.forEach((div)=> div.remove()); // Clears any existing squares
     board.style.gridTemplateColumns = `repeat(${size}, 1fr)` // makes "size" columns, and each column will have a width of 1/16th of the width of the container 
     board.style.gridTemplateRows = `repeat(${size}, 1fr)` // same as above but for rows
 
-    // Creates 256 squares for the board grid and inserts them
-    for (let i = 0; i<256; i++)
+    // Creates size^2 squares for the board grid and inserts them
+    let amount = size*size;
+    for (let i = 0; i<amount; i++)
     {
         let square = document.createElement('div');
         square.style.backgroundColor = 'blue';
@@ -13,9 +20,18 @@ function populateBoard(size)
     }
 }
 
-populateBoard(16);
+populateBoard(16); // Default
 
+// *************************************************
+//         Lets the user choose the board size
+// *************************************************
 function changeSize(input)
 {
-    populateBoard(input);
+    if (input >= 2 && input <= 100)
+    {
+        populateBoard(input);
+    }
+    else
+        console.log("invalid size");
+    
 }
